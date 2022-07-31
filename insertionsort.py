00df4554dfce3bf71b05e248c9efac4fdf8544e1
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2022-07-31 17:03:30 -0300

    Insertion Sorting Algorithm

--- insertionsort.py
+++ insertionsort.py
@@ -5,9 +5,8 @@ def InsertionSort(arr: list) -> list:
     for i in range(1, len(arr)):
         key = arr[i]
 
-        # Move elements of arr[0... i - 1], that are 
-        # greater than key, to one position ahead
-        # of their current position
+        # Move elements of arr[0... i - 1], that are greater than
+        # key, to one position ahead of their current position
         j = i - 1
 
         while j >= 0 and key < arr[j]:
